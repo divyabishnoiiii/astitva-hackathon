1846e8e86981fbf39542b810cfc81f9a9024e612
divyabishnoiiii <divyabishnoiigdtu@gmail.com>
2026-03-26 00:13:16 +0530

    Final integration: frontend + maps + housing + UI

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,7 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // your config (same as yours)
 const firebaseConfig = {
-  apiKey: "AIzaSyBJw12KetC7XEIP9JymuL3PQe5R6Q5aJY0",
+  
   authDomain: "astitva-d2bee.firebaseapp.com",
   projectId: "astitva-d2bee",
   storageBucket: "astitva-d2bee.firebasestorage.app",
